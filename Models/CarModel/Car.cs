9fbf6a0ba292ea8e8004c0921ae1b4cb6a1cb8ee
Viiccu <vbragari17@gmail.com>
2023-01-18 12:17:21 +0200

    Added some data in db

--- Models/CarModel/Car.cs
+++ Models/CarModel/Car.cs
@@ -14,7 +14,7 @@ namespace CarShopOnline_v3.Models.CarModel
         public int HorsePower { get; set; }
         public string FuelType { get; set; } = null!;
         public string Body { get; set; } = null!;
-        public string Photo { get; set; } = null!;
+        public string? Photo { get; set; } = null!;
         public string Description { get; set; } = null!;
         public int Price { get; set; }
         public string Contact { get; set; } = null!;
