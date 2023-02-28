a9dd834534292001f2c266bfe830ca0905fef619
Viiccu <vbragari17@gmail.com>
2023-02-28 10:42:36 +0200

    Updated the FontAwesome Version and made the logout button visible only on the profile page

---
+++ Migrations/CarShopDb/20230213113256_UpdatedImageModelTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarShopOnline_v3.Migrations.CarShopDb
+{
+    public partial class UpdatedImageModelTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "CarImages",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CarImages",
+                table: "CarImages",
+                column: "Image");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CarImages",
+                table: "CarImages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "CarImages",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
